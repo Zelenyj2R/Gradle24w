3775abeb11a25eca861542b4f6fccc1a0042cfc9
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-04-10 11:26:03 +0200

    Initial commit

---
+++ src/main/java/Gradle/Main.java
@@ -0,0 +1,12 @@
+package Gradle;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(new Person()));
+    }
+
+    }
